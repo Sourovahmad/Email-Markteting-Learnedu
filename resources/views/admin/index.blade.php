8177a7fe2e76d37d976d9c2fba7279b3ff13e984
sourovahmad <mdsourovahmad19@gmail.com>
2021-09-02 20:48:10 +0600

    admin mode

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,48 @@
-@extends('admin.layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-<p> hello this is admin</p>
+    <title>LearnEdu Emails</title>
+  </head>
+  <body>
 
 
-<script>
-    alert()
-</script>
-@endsection
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Email</th>
+      <th scope="col">Created At</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    @foreach ($data as $item)
+    <tr>
+      <th scope="row"> {{ $loop->iteration }}</th>
+      <td>{{ $item->mail }}</td>
+      <td>{{ $item->created_at }}</td>
+    </tr>
+    @endforeach
+
+
+
+
+
+  </tbody>
+</table>
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
